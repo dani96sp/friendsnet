f265ed7c405fe9acfcf91758735b28dd6e18e130
Dani <d.dani96.d@gmail.com>
2017-09-14 20:51:10 +0200

    arrancando con el proyecto final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { HistorietasComponent } from './historietas/historietas.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { ColegasComponent } from './colegas/colegas.component';
+import { EventosComponent } from './eventos/eventos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    HistorietasComponent,
+    PerfilComponent,
+    ColegasComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule
