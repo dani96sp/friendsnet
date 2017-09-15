ccbe9bb1a11ce4247d6a18fa2105a6ae8bdfe7b7
Dani <d.dani96.d@gmail.com>
2017-09-15 12:48:35 +0200

    poca cosa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HistorietasComponent } from './historietas/historietas.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FriendsComponent } from './friends/friends.component';
+import { EventsComponent } from './events/events.component';
+import { FootComponent } from './foot/foot.component';
+import { NavComponent } from './header/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HistorietasComponent,
+    ProfileComponent,
+    FriendsComponent,
+    EventsComponent,
+    FootComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
